0b8302f5c8b6afbb775358907bf675b410170cba
wtfshivam <Shivamsejwal123@gmail.com>
2025-04-26 17:52:27 +1000

    Added path and defined webpages in views.py (still have to refine the webpage and the look)

--- LifeFlow/main/views.py
+++ LifeFlow/main/views.py
@@ -5,3 +5,9 @@ def login(request):
 
 def calender(request):
     return render(request, 'calender.html')
+
+def Subscription(request):
+    return render(request, 'Subscription.html')
+
+def TaskManager(request):
+    return render(request, 'TaskManager.html')
